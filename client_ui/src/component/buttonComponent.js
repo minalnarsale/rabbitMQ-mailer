bbba942f45c1ecf72448d197ff879e761cf7ed98
minal <minalnarsale@gmail.com>
2020-05-02 00:15:45 +0530

    1) sending contact request to an emailId, 2) saving the status message in rabbitMQ whether mail got sent

---
+++ client_ui/src/component/buttonComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class Button extends React.Component {
+    constructor(){
+        super()
+    }
+    render() {
+        return(
+            <button>Submit</button>
+        )
+    }
+};
+
+export default Button;
