a6b1ec7fb207e58cf5558676e763e3d8704b661c
Suraj Narsale <surajsnarsale@gmail.com>
2021-10-19 20:31:50 +0530

    feat: add deploy settings

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use((req, res, next) => {
 
 if ((process.env.NODE_ENV = 'production')) {
 	app.use(express.static('client/build'));
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sentFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
 }
 
 // API calls
