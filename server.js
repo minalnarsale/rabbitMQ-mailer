dfa64901e3f1df01e93d15803d93c63c9a6fbfb1
Suraj Narsale <surajsnarsale@gmail.com>
2021-10-19 19:45:44 +0530

    feat: add project for  production

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const sendMail = require('./utility/queues/sendMail');
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -12,11 +13,15 @@ app.use(bodyParser.json());
 
 //middleware: adding logger for each request coming to server
 app.use((req, res, next) => {
-    console.log(new Date(), req.method, (decodeURIComponent(req.url)));
-    next();
+	console.log(new Date(), req.method, decodeURIComponent(req.url));
+	next();
 });
+
+if ((process.env.NODE_ENV = 'production')) {
+	app.use(express.static('client/build'));
+}
 
 // API calls
 app.post('/api/sendEmail', (req, res) => sendMail(req, res));
 
-app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${process.env.PORT}`));
